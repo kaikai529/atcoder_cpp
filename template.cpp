641135d189cb2b1d447403bacba7e6a18aad4b2c
kaikai529 <kaishi.19990513@icloud.com>
2022-10-29 00:49:04 +0900

    template

--- template.cpp
+++ template.cpp
@@ -1,1 +1,22 @@
-"""aiueo"""
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <ctime>
+#include <functional>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <tuple>
+#include <cassert>
+#include <bitset>
+
+using namespace std;
+
+int main(void){
+
+
+    return EXIT_SUCCESS;
+}
