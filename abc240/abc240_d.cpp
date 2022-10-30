8a0a3cfc51b72949029f62fb57a38c33decb48fa
kaikai529 <kaishi.19990513@icloud.com>
2022-10-30 20:40:34 +0900

    abc240

--- abc240/abc240_d.cpp
+++ abc240/abc240_d.cpp
@@ -2,7 +2,33 @@
 using namespace std;
 
 int main(void){
+    int n;
+    cin >> n;
 
+    int cnt = 0;
+    vector<pair<int,int>> ball;
+    for(int i=0;i<n;++i){
+        int a;
+        cin >> a;
+        cnt+=1;
+        // 前のボールを確認
+        if(ball.empty() or ball.back().first!=a){
+            ball.emplace_back(a,1);
+        } else {
+            ball.back().second += 1;
+            if(ball.back().second == a){
+                cnt -= a;
+                ball.pop_back();
+            }
+        }
+        cout << cnt << endl;
+    }
+    
 
     return EXIT_SUCCESS;
 }
+
+// 代入にも種類がある
+// - emplace_back
+// - emplace
+// - push_back
