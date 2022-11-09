546f70523d474d1d801ffdc5c46d57427701c5d9
kaikai529 <kaishi.19990513@icloud.com>
2022-11-09 19:10:36 +0900

    template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(val_name,start,end) = for(int i=j;i<k;j++);
+#define rep(val_name,start,end) for(ll val_name=(ll)start;val_name<end; val_name++)
 
 
 int main(void){
