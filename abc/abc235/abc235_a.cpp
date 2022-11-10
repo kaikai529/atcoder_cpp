960b78ac3378b8ba0f6347ad2d27fea0d3f72501
kaikai529 <kaishi.19990513@icloud.com>
2022-11-10 20:31:09 +0900

    move

---
+++ abc/abc235/abc235_a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(val_name,start,end) for(ll val_name=(ll)start;val_name<end; val_name++)
+
+
+int main(void){
+    char a, b, c;
+    cin >> a >> b >> c;
+    string abc = string({a, b, c});
+    // cout << atoi(a+b+c) + atoi(b+c+a)+ atoi(c+a+b);
+    return EXIT_SUCCESS;
+}
---
+++ abc/abc235/abc235_c.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(val_name,start,end) for(ll val_name=(ll)start;val_name<end; val_name++)
+
+
+int main(void){
+
+
+    return EXIT_SUCCESS;
+}
