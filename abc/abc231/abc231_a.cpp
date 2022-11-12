60b218b564d56063506c22bf402b12962234c0af
kaikai529 <kaishi.19990513@icloud.com>
2022-11-12 18:00:18 +0900

    abc231

--- abc/abc231/abc231_a.cpp
+++ abc/abc231/abc231_a.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 using ll = long long;
 #define rep(val_name,start,end) for(ll val_name=(ll)start;val_name<end; val_name++)
-#define mv(vec) for(auto &v:vec){cin >> v;}
+
+int ctoi(char c) {return c - '0';}
 
 int main(void){
+    int x;
+    cin >> x;
 
-
+    cout << fixed << setprecision(3) << (float)x / 100.;
     return EXIT_SUCCESS;
 }
--- abc/abc231/abc231_d.cpp
+++ abc/abc231/abc231_d.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 using ll = long long;
 #define rep(val_name,start,end) for(ll val_name=(ll)start;val_name<end; val_name++)
-#define mv(vec) for(auto &v:vec){cin >> v;}
+
+int ctoi(char c) {return c - '0';}
 
 int main(void){
 
